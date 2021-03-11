aebb7dfcd7d898f63492f505c4959acc103343e6
Shashwat Singh <66710757+SSingh087@users.noreply.github.com>
2021-03-11 12:25:07 +0530

    changes for np.array

--- lensGW/waveform/waveform_utils.py
+++ lensGW/waveform/waveform_utils.py
@@ -86,7 +86,7 @@ class lens_waveform_model(object):
         return self.eval_param(y0,y1,l0,l1,zS,zL,mL,lens_model_list)
         
     def eval_param(self,y0,y1,l0,l1,zS,zL,mL,lens_model_list):
-        mL,l0,l1 = array(mL,dtype=float64), array(l0,dtype=float64), array(l1,dtype=float64)
+        mL,l0,l1 = np.array(mL,dtype=float64), np.array(l0,dtype=float64), np.array(l1,dtype=float64)
         if len(mL)>1:
             mtot = sum(mL)
             thetaE  = param_processing(zL, zS, mtot)
